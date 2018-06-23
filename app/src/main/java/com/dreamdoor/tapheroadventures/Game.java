904ee9622da064eb04421daec934ac45db545100
IstvanMk <troian_steven@yahoo.com>
2018-06-23 21:29:49 +0300

    Starting to add mobs.

--- app/src/main/java/com/dreamdoor/tapheroadventures/Game.java
+++ app/src/main/java/com/dreamdoor/tapheroadventures/Game.java
@@ -5,20 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class Game extends AppCompatActivity {
 
-    TextView tv_time, tv_clicks, tv_zone;
-    Button b_click, b_start;
+    TextView tv_time, tv_zone, tv_gold;
+    Button b_click;
+    ImageButton imageButton;
 
     CountDownTimer timer;
     int time = 30;
-
-    int clicks = 0;
-
     int zoneM = 1;
     int zoneB = 1;
+    int gold = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,49 +26,47 @@ public class Game extends AppCompatActivity {
         setContentView(R.layout.activity_game);
 
         tv_time = (TextView) findViewById(R.id.tv_time);
-        tv_clicks = (TextView) findViewById(R.id.tv_clicks);
         tv_zone = (TextView) findViewById(R.id.tv_zone);
+        tv_gold = (TextView) findViewById(R.id.tv_gold);
         b_click = (Button) findViewById(R.id.b_click);
-        b_start = (Button) findViewById(R.id.b_start);
+        imageButton = (ImageButton) findViewById(R.id.imageButton);
+
+        //set gold to 0
+        tv_gold.setText("Gold: " + gold);
+
 
         // Start at zone 1 of world 1
 
         tv_zone.setText("Zone " + zoneM + '-' + zoneB);
-
-        b_start.setEnabled(true);
         b_click.setEnabled(false);
+
+        //adding the mobs
+
+        class Mobs {
+            private String name;
+            private String icon;
+            private int health;
+        }
+
 
         timer = new CountDownTimer(30000, 1000) {
             @Override
             public void onTick(long l) {
-                time --;
+                time--;
                 tv_time.setText("Time: " + time);
             }
 
             @Override
             public void onFinish() {
-                b_start.setEnabled(true);
                 b_click.setEnabled(false);
                 tv_time.setText("Time: 0");
             }
         };
-        b_click.setOnClickListener(new View.OnClickListener(){
+        b_click.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick (View v) {
-                clicks++;
-                tv_clicks.setText("Clicks: " + clicks);
-            }
-        });
-        b_start.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick (View v) {
-                timer.start();
-                b_start.setEnabled(false);
-                b_click.setEnabled(true);
-                clicks = 0;
-                time = 30;
-                tv_time.setText("Time: " + time);
-                tv_clicks.setText("Clicks: " + clicks);
+            public void onClick(View v) {
+                gold++;
+                tv_gold.setText("Gold: " + gold);
             }
         });
     }
