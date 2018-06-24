9fa32be7191240260dbe98eed71ec85cd3563521
IstvanMk <troian_steven@yahoo.com>
2018-06-24 14:42:15 +0300

    Random mobs per click.

--- app/src/main/java/com/dreamdoor/tapheroadventures/Game.java
+++ app/src/main/java/com/dreamdoor/tapheroadventures/Game.java
@@ -1,5 +1,6 @@
 package com.dreamdoor.tapheroadventures;
 
+import android.media.Image;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,6 +8,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import java.util.Random;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Game extends AppCompatActivity {
 
@@ -30,6 +34,8 @@ public class Game extends AppCompatActivity {
         tv_gold = (TextView) findViewById(R.id.tv_gold);
         b_click = (Button) findViewById(R.id.b_click);
         imageButton = (ImageButton) findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(imgButtonHandler);
+
 
         //set gold to 0
         tv_gold.setText("Gold: " + gold);
@@ -42,11 +48,6 @@ public class Game extends AppCompatActivity {
 
         //adding the mobs
 
-        class Mobs {
-            private String name;
-            private String icon;
-            private int health;
-        }
 
 
         timer = new CountDownTimer(30000, 1000) {
@@ -70,4 +71,30 @@ public class Game extends AppCompatActivity {
             }
         });
     }
+    View.OnClickListener imgButtonHandler = new View.OnClickListener() {
+
+        public void onClick(View v) {
+            int[] MobArray = new int[10];{
+                MobArray[0] = R.drawable.aerocephal;
+                MobArray[1] = R.drawable.arcana_drake;
+                MobArray[2] = R.drawable.aurumdrakueli;
+                MobArray[3] = R.drawable.bat;
+                MobArray[4] = R.drawable.daemarbora;
+                MobArray[5] = R.drawable.deceleon;
+                MobArray[6] = R.drawable.demonic_essence;
+                MobArray[7] = R.drawable.dune_crawler;
+                MobArray[8] = R.drawable.green_slime;
+                MobArray[9] = R.drawable.nagaruda;
+                MobArray[9] = R.drawable.rat;
+                MobArray[9] = R.drawable.scorpion;
+                MobArray[9] = R.drawable.skeleton;
+                MobArray[9] = R.drawable.spider;
+                MobArray[9] = R.drawable.stygian_lizard;
+            }
+            Random generator = new Random();
+            int randomIndex = generator.nextInt(MobArray.length);
+
+            imageButton.setImageResource(MobArray[randomIndex]);
+        }
+    };
 }
